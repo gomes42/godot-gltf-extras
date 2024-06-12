6cede9bd5cac581a799a33b10c9b8cecb2543b02
Lucas <lgxm3z@gmail.com>
2024-06-12 17:47:19 -0300

    update script

--- addons/gltf-extras/gltf-extras.gd
+++ addons/gltf-extras/gltf-extras.gd
@@ -18,4 +18,47 @@ class ExtrasImporter extends GLTFDocumentExtension:
 		return typeof(v) == TYPE_ARRAY and v.size() == 3
 
 	func _import_post(state: GLTFState, root: Node) -> Error:
+
+		# print(state.json)
+
+		var lights_json : Array = state.json.get("extensions", {}).get("KHR_lights_punctual", {}).get("lights", [])
+		var materials_json : Array = state.json.get("materials", [])
+
+		var nodes_json : Array = state.json.get("nodes", [])
+		for i in nodes_json.size():
+			var node = state.get_scene_node(i)
+			if !node:
+				continue
+
+			if node is Light3D:
+				# clear all meta
+				for key in node.get_meta_list():
+					node.set_meta(key, null)
+
+				var light = lights_json[nodes_json[i].get("extensions", {}).get("KHR_lights_punctual", {}).get("light", -1)]	
+				
+				for key in light.get("extras", {}).keys():
+					node.set(key, light.get("extras", {}).get(key))
+
+			if node is ImporterMeshInstance3D:
+
+				var arrayMesh: ArrayMesh = node.mesh.get_mesh();
+
+				for j in arrayMesh.get_surface_count():
+					var material:Material = arrayMesh.surface_get_material(j)
+
+					# clear all meta
+					for key in material.get_meta_list():
+						material.set_meta(key, null)
+					
+					for _m in materials_json.size():
+						if materials_json[_m].get("name") == material.resource_name:
+							for extras in materials_json[_m].get("extras", {}).keys():
+								
+								if is_vector3(materials_json[_m].get("extras", {}).get(extras)):
+									material.set(extras, Vector3(materials_json[_m].get("extras", {}).get(extras)[0], materials_json[_m].get("extras", {}).get(extras)[1], materials_json[_m].get("extras", {}).get(extras)[2]))
+								else :
+									material.set(extras, materials_json[_m].get("extras", {}).get(extras))
+
+
 		return OK
